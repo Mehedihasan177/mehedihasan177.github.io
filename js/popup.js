9701f349870761f83a0a249dd589e3e6015de305
Mehedi <mdmehedihasan4051@gmail.com>
2023-09-18 08:31:29 +0600

    close button need to work

--- js/popup.js
+++ js/popup.js
@@ -1,21 +1,26 @@
-// Get references to the image and the popup elements
-var image = document.getElementById('myImage');
-var overlay = document.getElementById('imageOverlay');
-var closePopup = document.getElementById('closePopup');
+var modal = document.getElementById("myModal");
+        var modalImage = document.getElementById("modalImage");
 
-// Function to open the popup
-image.onclick = function() {
-    overlay.style.display = 'block';
-}
+        // Get the text link that opens the modal
+        var openModalLink = document.getElementById("openModal");
 
-// Function to close the popup when clicking the close button
-closePopup.onclick = function() {
-    overlay.style.display = 'none';
-}
+        // Get the close button in the modal
+        var closeModalButton = document.getElementById("closeModal");
 
-// Function to close the popup when clicking outside the popup
-overlay.onclick = function(event) {
-    if (event.target === overlay) {
-        overlay.style.display = 'none';
-    }
-}
+        // When the user clicks the text link, open the modal and set the image source
+        openModalLink.addEventListener("click", function() {
+            modal.style.display = "block";
+            modalImage.src = "images/doctor_mediaid.png";
+        });
+
+        // When the user clicks the close button, close the modal
+        closeModalButton.addEventListener("click", function() {
+            modal.style.display = "none";
+        });
+
+        // When the user clicks outside the modal, close it
+        window.addEventListener("click", function(event) {
+            if (event.target === modal) {
+                modal.style.display = "none";
+            }
+        });
